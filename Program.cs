6856521d7ecc36b5d6e57b2c209ed6505ca1547e
Yakup Layık <yakuplayik@hotmail.com>
2022-02-02 23:16:19 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(Convert.ToInt32(sonuc));
+
+            Metot ornek = new Metot();
+            ornek.EkranaYazdir(sonuc.ToString());
+
+            int sonuc2 = ornek.ArttirTopla(ref a, ref b);
+            ornek.EkranaYazdir(sonuc2.ToString());
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    class Metot
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirTopla(ref int x,ref int y)
+        {
+            x++;
+            y++;
+            return x + y; 
+        }
+    }
+}
